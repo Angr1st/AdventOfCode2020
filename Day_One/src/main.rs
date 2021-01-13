a718dee69655f95ec2cba8f840f2c3c13d08875e
Torben Clasen <t.clasen@outlook.com>
2021-01-13 20:59:55 +0100

    First Day following the fasterthanli

---
+++ Day_One/src/main.rs
@@ -0,0 +1,18 @@
+use itertools::Itertools;
+
+fn main() -> anyhow::Result<()> {
+   let (a, b, c) = include_str!("input.txt")
+         .lines()
+         .map(str::parse::<i64>)
+         .collect::<Result<Vec<_>, _>>()?
+         .into_iter()
+         .tuple_combinations()
+         .find(|(a,b, c)| a + b + c== 2020)
+         .expect("no tiplet had a sum of 2020")
+      ;
+
+   dbg!(a + b + c);
+   dbg!(a * b * c);
+
+   Ok(())
+}
